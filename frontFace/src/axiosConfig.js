a2fff82987a1e855ef7778f8fb86e2227ce57537
rahmet <155022635+rameto-shifa@users.noreply.github.com>
2024-07-01 14:02:24 +0300

    axios

--- frontFace/src/axiosConfig.js
+++ frontFace/src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosBase = axios.create({
     // baseURL: 'http://localhost:5500/api'
-    baseURL: 'https://evangagi-forum-deploy-backend.onrender.com'
+    baseURL : 'https://evangagi-forum-deploy-backend.onrender.com'
 })
 
 export default axiosBase 
